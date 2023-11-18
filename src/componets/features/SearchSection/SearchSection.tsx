1502eb21d4f259f8d98bc79732c50d05f1465586
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-11-18 20:46:19 +0400

    fix: fix search parametrs bug

--- src/componets/features/SearchSection/SearchSection.tsx
+++ src/componets/features/SearchSection/SearchSection.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { getDataLocalStorage } from '../../shared/lib/localStorage';
+import { useAppDispatch } from '../../App/store/hooks';
 
 import styles from './SearchSection.module.css';
 import Input from './ui/Input';
 import Button from './ui/Button';
 import useCharacterSearch from './hooks/useCharacterSearch';
+import useStartedSearchParams from './hooks/useStartedSearchParams';
+import { setValue } from './searchSectionSlice';
 
 const SearchSection = () => {
+  const dispatch = useAppDispatch();
   const [inputValue, setInputValue] = useState(
     getDataLocalStorage('characterSearch')
   );
   const { getAllCharacters } = useCharacterSearch(inputValue);
+  const { page } = useStartedSearchParams();
 
   const handleClick = () => getAllCharacters();
 
@@ -23,6 +28,10 @@ const SearchSection = () => {
 
   const onHandleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
     setInputValue(e.target.value);
+
+  useEffect(() => {
+    dispatch(setValue({ key: 'currentPage', value: page }));
+  }, []);
 
   return (
     <div className={styles.searchContainer}>
--- src/componets/features/SearchSection/hooks/useCharacterSearch.tsx
+++ src/componets/features/SearchSection/hooks/useCharacterSearch.tsx
@@ -1,18 +1,21 @@
 import setDataLocalStorage from '../../../shared/lib/localStorage';
 import { setValue } from '../searchSectionSlice';
 import { useAppDispatch } from '../../../App/store/hooks';
-import { LOCAL_STORAGE_KEY } from '../../../shared/constants/constants';
+import {
+  DEFAULT_PAGE,
+  LOCAL_STORAGE_KEY,
+} from '../../../shared/constants/constants';
 
 import useStartedSearchParams from './useStartedSearchParams';
 
 const useCharacterSearch = (res: string) => {
-  const { page, setInitSearchParams } = useStartedSearchParams();
+  const { setInitSearchParams } = useStartedSearchParams();
   const dispatch = useAppDispatch();
   const getAllCharacters = () => {
     setDataLocalStorage(LOCAL_STORAGE_KEY, res);
     setInitSearchParams();
     dispatch(setValue({ key: 'inputValue', value: res.trim() }));
-    dispatch(setValue({ key: 'currentPage', value: page }));
+    dispatch(setValue({ key: 'currentPage', value: DEFAULT_PAGE }));
   };
 
   return { getAllCharacters };
