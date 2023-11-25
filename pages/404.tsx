d0602f642e51a743a5d7e0d690f54ca0bab852c0
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-11-25 21:46:03 +0400

    fix: fix 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,15 +1,29 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import smoke from '@/public/assets/images/smoke.png';
 import logo from '@/public/assets/images/logo.png';
 import ErrorButton from '@/components/shared/UI/ErrorButton/ErrorButton';
 import NotFound from '@/components/shared/UI/NotFound/NotFound';
+import {
+  DEFAULT_CARDS,
+  DEFAULT_PAGE,
+} from '@/components/shared/constants/constants';
 
 import headerStyles from '../components/widgets/Header/Header.module.css';
 import styles from '../styles/404.module.css';
 
 const NotFoundPage = () => {
+  const router = useRouter();
+
+  const { page, value, size } = router.query;
+
+  const [newPage, newValue, newSize] = [
+    page ?? DEFAULT_PAGE,
+    value ?? '',
+    size ?? DEFAULT_CARDS,
+  ];
   return (
     <>
       <header className={headerStyles.header}>
@@ -21,7 +35,10 @@ const NotFoundPage = () => {
       </header>
       <main className={styles.main}>
         <NotFound />
-        <Link href="/" className={styles.button}>
+        <Link
+          href={`/?page=${newPage}&value=${newValue}&size=${newSize}`}
+          className={styles.button}
+        >
           Go To Main
         </Link>
       </main>
