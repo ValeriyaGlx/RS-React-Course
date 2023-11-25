8bf029480906f8afa552157f471a4e299cfda56c
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-11-25 23:31:17 +0400

    feat: implement loaders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import NextNProgress from 'nextjs-progressbar';
 
 import Providers from '@/components/widgets/store/Provider';
 import ErrorBoundary from '@/components/shared/UI/ErrorBoundary/ErrorBoundary';
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary>
       <Providers>
+        <NextNProgress color="#e0bc09" />
         <Component {...pageProps} />
       </Providers>
     </ErrorBoundary>
