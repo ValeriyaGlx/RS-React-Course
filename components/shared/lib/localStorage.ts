2d3d89211f8703d2e7ff2d81bccd2468e418f533
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-11-26 06:42:52 +0400

    fix: remove unuseful ls functions

--- components/shared/lib/localStorage.ts
+++
@@ -1,7 +0,0 @@
-export const setDataLocalStorage = (name: string, data: string) => {
-  localStorage.setItem(name, data.trim());
-};
-
-export const getDataLocalStorage = (name: string) => {
-  return typeof window !== 'undefined' ? localStorage.getItem(name) ?? '' : '';
-};
