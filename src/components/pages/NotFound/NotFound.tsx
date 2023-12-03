8506eb74b46d453bd421ebd9eafd362133549eae
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-12-04 01:25:44 +0400

    fix: fix 404 page

--- src/components/pages/NotFound/NotFound.tsx
+++ src/components/pages/NotFound/NotFound.tsx
@@ -1,14 +1,10 @@
 import { Link } from 'react-router-dom';
 
-import styles from './NotFound.module.css';
-
 const NotFound = () => {
   return (
     <>
       <h2>Oh dear. Are you lost?</h2>
-      <Link className={styles.button} to="/">
-        Go to Main
-      </Link>
+      <Link to="/">Go to Main</Link>
     </>
   );
 };
